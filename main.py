178ebb7147f2c75fa3cc3749ef8243a5cb362ee8
GerrardXiong <chengcheng.xiong@my.jcu.edu.au>
2022-03-08 01:11:45 +1000

    Move player to bottom of the screen Add Right and left key for player

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-import pygame, sys
+import pygame
+import sys
 
 from player import Player
 
+
 class Game:
     def __init__(self):
-        player_sprite = Player((300,300))
+        player_sprite = Player((screen_width/2, screen_height))
         self.player = pygame.sprite.GroupSingle(player_sprite)
 
-    def run(self): 
+    def run(self):
+        self.player.update()
         self.player.draw(screen)
     # update all sprite groups
     # draw all sprite groups
-    
+
 
 if __name__ == '__main__':
     pygame.init()
--- player.py
+++ player.py
@@ -1,3 +1,4 @@
+from typing_extensions import Self
 import pygame,os
 
 class Player(pygame.sprite.Sprite):
@@ -5,3 +6,15 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.image.load('/Users/gerrardxcc/Desktop/AppDev/Space-Invaders/images/player.png').convert_alpha()
         self.rect = self.image.get_rect(midbottom = pos)
+        self.speed = 5
+
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_RIGHT]:
+            self.react.x += self.speed
+        elif keys[pygame.K_LEFT]:
+            self.react.x -= self.speed
+
+    def update(self):
+        self.get_input()
