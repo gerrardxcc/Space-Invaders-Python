84e9a53f3cacb828a519b341c6ffee881e2f02db
GerrardXiong <chengcheng.xiong@my.jcu.edu.au>
2022-03-08 18:42:37 +1000

    add one obstacle and relocated from top to bottom

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ class Game:
         self.shape = obstacle.shape
         self.block_size = 6
         self.blocks = pygame.sprite.Group()
-        self.create_obstacle()
+        self.create_obstacle(40, 480)
 
-    def create_obstacle(self):
+    def create_obstacle(self, x_start, y_start):
         for row_index, row in enumerate(self.shape):
             for col_index, col in enumerate(row):
                 if col == 'x':
-                    x = col_index * self.block_size
-                    y = row_index * self.block_size
+                    x = x_start + col_index * self.block_size
+                    y = y_start +  row_index * self.block_size
                     block = obstacle.Block(self.block_size,(241,79,80),x,y)
                     self.blocks.add(block)
 
