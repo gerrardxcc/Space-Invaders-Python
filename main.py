b5d0d70b02e0431af45d334f76a71c8e271d8015
GerrardXiong <chengcheng.xiong@my.jcu.edu.au>
2022-03-08 22:45:14 +1000

    add mutiple obstacles

--- main.py
+++ main.py
@@ -1,32 +1,40 @@
 from tkinter import Y
 from numpy import block
-import pygame, sys
+import pygame
+import sys
 
 from player import Player
-import obstacle 
+import obstacle
+
 
 class Game:
     def __init__(self):
         # player setup
-        player_sprite = Player((screen_width/2, screen_height),screen_width,5  )
+        player_sprite = Player(
+            (screen_width/2, screen_height), screen_width, 5)
         self.player = pygame.sprite.GroupSingle(player_sprite)
 
         # obstacle setup
         self.shape = obstacle.shape
         self.block_size = 6
         self.blocks = pygame.sprite.Group()
-        self.create_obstacle(40, 480)
+        self.obstacle_amount = 4
+        self.obstacle_x_positions = [ num * (screen_width / self.obstacle_amount) for num in range(self.obstacle_amount)]
+        self.create_multiple_obstacle(*self.obstacle_x_positions, x_start = screen_width / 15, y_start=480)
 
-    def create_obstacle(self, x_start, y_start):
+    def create_obstacle(self, x_start, y_start, offset_x):
         for row_index, row in enumerate(self.shape):
             for col_index, col in enumerate(row):
                 if col == 'x':
-                    x = x_start + col_index * self.block_size
-                    y = y_start +  row_index * self.block_size
-                    block = obstacle.Block(self.block_size,(241,79,80),x,y)
+                    x = x_start + col_index * self.block_size + offset_x
+                    y = y_start + row_index * self.block_size
+                    block = obstacle.Block(
+                        self.block_size, (241, 79, 80), x, y)
                     self.blocks.add(block)
 
-
+    def create_multiple_obstacle(self, *offset, x_start, y_start):
+        for offset_x in offset:
+            self.create_obstacle(x_start, y_start, offset_x)
 
     def run(self):
         self.player.update()
