cb0836d53850d4f1156da0316aff4e964927fdf1
GerrardXiong <chengcheng.xiong@my.jcu.edu.au>
2022-03-09 16:19:23 +1000

    add font style and score system

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ class Game:
         self.lives = 3
         self.live_surf = pygame.image.load('/Users/gerrardxcc/Desktop/AppDev/Space-Invaders/images/player.png').convert_alpha()
         self.live_x_start_pos = screen_width - (self.live_surf.get_size()[0] * 2 + 20)
+        self.score = 0
+        self.font = pygame.font.Font('/Users/gerrardxcc/Desktop/AppDev/Space-Invaders/font/Pixeled.ttf',28)
 
         # Obstacle setup
         self.shape = obstacle.shape
@@ -96,12 +98,16 @@ class Game:
                     laser.kill()
 
                 # alien collisions
-                if pygame.sprite.spritecollide(laser,self.aliens,True):
+                aliens_hit = pygame.sprite.spritecollide(laser,self.aliens,True)
+                if aliens_hit:
+                    for alien in aliens_hit:
+                        self.score += alien.value
                     laser.kill()
 
                 # extra collision
                 if pygame.sprite.spritecollide(laser,self.extra,True):
                     laser.kill()
+                    self.score += 500
 
         # alien lasers
         if self.alien_lasers:
@@ -132,26 +138,29 @@ class Game:
             x = self.live_x_start_pos + (live * (self.live_surf.get_size()[0] + 10))
             screen.blit(self.live_surf,(x,8))
 
+    def display_score(self):
+        score_surf = self.font.render(f'score: {self.score}', False, 'white')
+        score_rect = score_surf.get_rect(topleft = (10,-10))
+        screen.blit(score_surf,score_rect)
     def run(self):
         self.player.update()
+        self.alien_lasers.update()
+        self.extra.update()
+
         self.aliens.update(self.alien_direction)
         self.alien_direction_checker()
-        self.alien_lasers.update()
         self.extra_alien_timer()
-        self.extra.update()
         self.collison_checks()
-        self.display_lives()
-
-
+        
         self.player.sprite.lasers.draw(screen)
         self.player.draw(screen)
-
         self.blocks.draw(screen)
         self.aliens.draw(screen)
         self.alien_lasers.draw(screen)
         self.extra.draw(screen)
-    # update all sprite groups
-    # draw all sprite groups
+        self.display_lives()
+        self.display_score()
+
 
 
 if __name__ == '__main__':
